31cc42f068b69425f4b779c70ae696a4eaff4291
NjFaiaz <njfaiaz@gmail.com>
2023-08-30 23:25:54 +0600

    Two way data binding

---
+++ day 04/app.js
@@ -0,0 +1,26 @@
+var app = Vue.createApp({
+    data(){
+        return{
+            num:"",
+            result:""
+        };
+    },
+
+    methods: {
+        getDouble(){
+            this.result = this.num * 2;
+        },
+        getSquare(){
+            this.result = this.num * this.num;
+        },
+        addTo(){
+            this.result = this.num + 10;
+        },
+        reset(){
+            this.num="";
+            this.result=""
+        }
+    }
+});
+
+app.mount("#app");
